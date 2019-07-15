98e4ed855299bf28e51f552b2cfbd925f20640f6
Brandon Smith <brandon.smith@tbsokc.com>
2019-07-15 11:02:14 -0500

    updated db

--- project/__init__.py
+++ project/__init__.py
@@ -6,12 +6,15 @@ from .models import db
 def create_app(config_file):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile(config_file, silent=True)
+    if environ.get('DATABASE_URL'):
+        app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 
     environ.update(
-         USER_ID = str(app.config['TWITCH_USER_ID']),
-         DATABASE_URI = app.config['SQLALCHEMY_DATABASE_URI']
+         USER_ID = str(app.config['TWITCH_USER_ID'])
     )
     
+    print(app.config)
+
     CORS(app)
 
     db.init_app(app)
